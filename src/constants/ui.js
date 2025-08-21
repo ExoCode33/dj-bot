b41cbb04e19fb2193fe0a1463aae4077a837b2fe
ExoCode33 <emmanuel@supracode.ca>
2025-08-21 17:58:35 -0400

    Create ui.js

---
+++ src/constants/ui.js
@@ -0,0 +1,13 @@
+export const UI = {
+  Buttons: {
+    Queue: 'uta_queue',
+    PlayPause: 'uta_playpause',
+    Skip: 'uta_skip'
+  },
+  Modals: {
+    QueueModal: 'uta_queue_modal'
+  },
+  Inputs: {
+    Query: 'uta_query'
+  }
+};
