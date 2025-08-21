865192bebd58daf2085164158ba19e4e97cb9c1e
ExoCode33 <emmanuel@supracode.ca>
2025-08-21 17:58:52 -0400

    Create client.js

---
+++ src/core/client.js
@@ -0,0 +1,14 @@
+import { Client, GatewayIntentBits, Collection } from 'discord.js';
+import { initLavalink } from '../features/music/lavalink.js';
+
+export function createClient() {
+  const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
+  });
+
+  client.commands = new Collection();
+  client.shoukaku = initLavalink(client);
+
+  import('./loader.js');
+  return client;
+}
