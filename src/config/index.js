791dd1ccd0a3cb4981729126d4b27591a667453d
ExoCode33 <emmanuel@supracode.ca>
2025-08-22 14:07:51 -0400

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -9,11 +9,14 @@ export const cfg = {
   lavalink: {
     name: process.env.LAVALINK_NAME || 'railway',
     url: process.env.LAVALINK_URL || 'localhost:2333',
-    auth: process.env.LAVALINK_AUTH || 'youshallnotpass',
-    secure: readEnvBool('LAVALINK_SECURE', false)
+    auth: process.env.LAVALINK_AUTH || 'UtaUtaDj',
+    secure: readEnvBool('LAVALINK_SECURE', true)
   },
   uta: {
     authorizedRoleId: process.env.AUTHORIZED_ROLE_ID || '',
     defaultVolume: readEnvNum('DEFAULT_VOLUME', 35)
+  },
+  difm: {
+    apiKey: process.env.DIFM_API_KEY || ''
   }
 };
